1b782bd7a39424f637660ccc08eeba68bde43efa
lrwhiting <lrw8@gneseo.edu>
2024-02-13 15:09:50 -0500

    links

---
+++ classnotes.R
@@ -0,0 +1,86 @@
+#Class work 5 
+{
+# Question 1 
+{
+    #In R, what are the different data types and data structures we discuss in
+    #our course? Provide an example of each.
+    
+    # data Type 
+    type_chr <- "a" # character
+    type_int <- 1L # integer
+    type_num <- 1.1 #numeric
+    type_logical <- True #logical
+    type_factor <- factor (1:5) #factor
+    
+    # Data structer 
+    vector_ex <- c(1,2,3) # vector
+    
+    library(tidyverse)
+    df_mpg <- ggplot::mpg # data frame
+    
+    
+}
+# Question 2
+{
+fare <- "10.00"
+tip <- "2.00"
+tax <- "0.80"
+
+fare <- as.numeric(fare)
+tip <- as.numeric(tip)
+tax <- as.numeric(tax)
+
+str_c("The total trip cost is:", "12.80")
+total <- (fare + tax + tip)
+st_c("The total trip cost is:", total,"0")
+print("The total trip cost is:", total,"0")
+}
+
+# Question 3 
+{#Read the following CSV file as an object, tips.
+#https://bcdanl.github.io/data/tips_seaborn.csv
+}
+
+#Question 4
+{#Use DT::datatable() for the tips data.frame .  
+
+  
+}
+
+#Question 5
+{
+#What are the minimum, minimum, median, mean and standard deviation of the 
+  # tip variable in the tips data.frame?
+  
+}
+  
+}
+#Class work 6
+{
+# Question 1. First Steps
+{
+ #Load the following data.frame, mpg.
+```r
+library(tidyverse)
+mpg <- ggplot2::mpg
+
+datatable(mpg)
+
+
+
+library(skimr)
+skim(mpg)
+
+```}
+}
+  
+
+
+
+
+
+
+
+
+
+  
