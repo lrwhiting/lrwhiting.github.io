c2d57fa27fcae49dab941346380f63a3d22f4160
lrwhiting <lrw8@gneseo.edu>
2024-02-13 13:55:10 -0500

    hope addpic

--- webtokens .R
+++ webtokens .R
@@ -2,8 +2,25 @@ install.packages("usethis")
 usethis::create_github_token()
 install.packages("gitcreds")
 
+#git add .
+#git commit -m "any message to describe the changes"
+#git push -u origin master
 
-gitcreds::gitcreds_set()
+gitcreds::gitcreds_set() # replace the token 
+usethis::create_github_token()  # to creat a brand new token 
+
+#tokens
+  # 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
+
+  # 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
+
+  # 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
+
 # quarto render
-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
-# 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
+
+
+fare <- "10.00"
+tip <- "2.00"
+tax <- "0.80"
+str_c(fare + tip + tax,sep="The total trip cost is:")
+      
