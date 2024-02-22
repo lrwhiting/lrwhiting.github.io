e0cfb48f5f3ad03dc0428048b28d72f8021f96e0
lrwhiting <lrw8@gneseo.edu>
2024-02-22 14:28:00 -0500

    HW1

--- webtokens .R
+++ webtokens .R
@@ -10,9 +10,6 @@ usethis::create_github_token()  # to creat a brand new token
 gitcreds::gitcreds_set() # replace the token 
 
 #tokens
-  # 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
-  # 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
-  # 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
-  # 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
+  # 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
 
 # quarto render
